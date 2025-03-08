064f922635d4907014f37f42c03c2652319b0c51
feat: Iniciando app.py e estrutura. <arthguins@gmail.com>
2025-03-08 00:07:56 -0300

    Simplify Page component layout by removing unnecessary wrapper div around Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,9 @@ const Page = () => {
   return (
     <div className="min-h-screen flex flex-col items-center">
       <Navbar />
-      <div className="flex-1 p-2 flex justify-center mt-12 md:mt-6 lg:mt-0 max-w-screen-lg w-full">
-        <div className="flex flex-wrap gap-4 md:gap-6 lg:gap-2 justify-center"> 
-          <Card />
+      <Card />
         </div>
-      </div>
-    </div>
+      
   );
 };
 
