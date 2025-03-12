08cd39a820b6225faceef91df20fc5225574cbe1
feat: Iniciando app.py e estrutura. <arthguins@gmail.com>
2025-03-12 14:16:59 -0300

    Translate README content to Portuguese and add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'media', // ou 'class' se preferir alternar manualmente
+  theme: {
+    extend: {
+      // suas configurações de tema personalizadas
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
